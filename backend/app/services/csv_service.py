c9efc5f62f4f9aabe2a326a0670f7211c8c87b2c
benitmatumona <polviedivine@gmail.com>
2026-06-04 22:55:18 +0200

    test

--- backend/app/services/csv_service.py
+++ backend/app/services/csv_service.py
@@ -2,10 +2,18 @@ import pandas as pd
 
 
 def analyze_csv(csv_path: str) -> dict:
-    
     df = pd.read_csv(csv_path)
+
+    if all(name != "Score" for name in df.to_dict().keys()):
+        return {
+                "success": False,
+                "error": "Column 'Score' not found"
+        }
     return {
-        "student_count": len(df),
-        "highest_score": df["Score"].max(),
-        "average_score": round(df["Score"].mean(), 2)
+        "success": True,
+        "data": {
+            "student_count": len(df),
+            "highest_score": df["Score"].max(),
+            "average_score": round(df["Score"].mean(), 2)
+        }
     }
